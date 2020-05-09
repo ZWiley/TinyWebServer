bb2dbef618702c542dd858c1fb5781f3d4043c6f
ZWiley <wiley97670693@gmail.com>
2020-05-09 18:10:36 +0800

    重构代码

--- makefile
+++ makefile
@@ -1,8 +1,8 @@
-server: main.c ./threadpool/threadpool.h ./http/http_conn.cpp ./http/http_conn.h ./lock/locker.h ./log/log.cpp ./log/log.h ./log/block_queue.h ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h
-	g++ -o server main.c ./threadpool/threadpool.h ./http/http_conn.cpp ./http/http_conn.h ./lock/locker.h ./log/log.cpp ./log/log.h ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h -lpthread -lmysqlclient
+server: main.c ./timer/lst_timer.h ./timer/lst_timer.cpp ./threadpool/threadpool.h ./http/http_conn.cpp ./http/http_conn.h ./lock/locker.h ./log/log.cpp ./log/log.h ./log/block_queue.h ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h webserver.h webserver.cpp config.h config.cpp
+	g++ -o server main.c ./timer/lst_timer.h ./timer/lst_timer.cpp ./threadpool/threadpool.h ./http/http_conn.cpp ./http/http_conn.h ./lock/locker.h ./log/log.cpp ./log/log.h ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h webserver.h webserver.cpp config.h config.cpp -lpthread -lmysqlclient
 
 CGISQL.cgi:./CGImysql/sign.cpp ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h
-	g++ -o ./root/CGISQL.cgi ./CGImysql/sign.cpp ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h -lmysqlclient
+	g++ -o ./root/CGISQL.cgi ./CGImysql/sign.cpp ./CGImysql/sql_connection_pool.cpp ./CGImysql/sql_connection_pool.h -lmysqlclient -lpthread
 
 
 clean:
--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -1,10 +1,3 @@
-/**
- * 半同步/半反应堆线程池：主线程监听listen socket和接收到的所有连接socket，当有客户端请求任务时，
- * 将任务对象插入到工作任务对象中；等待在任务队列上的工作线程通过竞争来取得任务对象并处理之。
- * 其中的工作任务队列完成了主线程与工作线程之间的解耦，
- * 但是由于同一客户连接的任务请求可能由不同的线程来处理，所以这要求所有的客户请求是无状态的。
-*/
-
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
@@ -25,8 +18,6 @@ public:
     bool append(T *request);
 
 private:
-    //注意： 当把线程函数封装在类中，this指针会作为默认的参数被传进函数中，从而和线程函数参数(void*)不能匹配。
-    //解决： 线程函数作为静态函数，因为在C++中静态函数没有this指针
     /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
     static void *worker(void *arg);
     void run();
@@ -38,11 +29,10 @@ private:
     std::list<T *> m_workqueue; //请求队列
     locker m_queuelocker;       //保护请求队列的互斥锁
     sem m_queuestat;            //是否有任务需要处理
-    bool m_stop;                //是否结束线程
     connection_pool *m_connPool;  //数据库
 };
 template <typename T>
-threadpool<T>::threadpool( connection_pool *connPool, int thread_number, int max_requests) : m_thread_number(thread_number), m_max_requests(max_requests), m_stop(false), m_threads(NULL),m_connPool(connPool)
+threadpool<T>::threadpool( connection_pool *connPool, int thread_number, int max_requests) : m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
 {
     if (thread_number <= 0 || max_requests <= 0)
         throw std::exception();
@@ -51,14 +41,11 @@ threadpool<T>::threadpool( connection_pool *connPool, int thread_number, int max
         throw std::exception();
     for (int i = 0; i < thread_number; ++i)
     {
-        //printf("create the %dth thread\n",i);
-        //创建线程（注意线程函数为静态成员函数，且将this指针作为参数，以访问进程池的成员变量）
         if (pthread_create(m_threads + i, NULL, worker, this) != 0)
         {
             delete[] m_threads;
             throw std::exception();
         }
-        //将工作线程设置为脱离线程（当脱离线程退出时，系统会自动回收该线程的资源），否则需要其他线程来回收该线程的资源
         if (pthread_detach(m_threads[i]))
         {
             delete[] m_threads;
@@ -70,23 +57,21 @@ template <typename T>
 threadpool<T>::~threadpool()
 {
     delete[] m_threads;
-    m_stop = true;
 }
 template <typename T>
 bool threadpool<T>::append(T *request)
 {
-    m_queuelocker.lock();//由于任务队列被所有线程共享，因此需要先加锁
-    if (m_workqueue.size() > m_max_requests)
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
     {
         m_queuelocker.unlock();
         return false;
     }
     m_workqueue.push_back(request);
     m_queuelocker.unlock();
-    m_queuestat.post();//将信号量+1，如果大于0，则唤醒等待该信号量的工作线程
+    m_queuestat.post();
     return true;
 }
-
 template <typename T>
 void *threadpool<T>::worker(void *arg)
 {
@@ -94,28 +79,27 @@ void *threadpool<T>::worker(void *arg)
     pool->run();
     return pool;
 }
-
-//工作线程的处理函数：从任务队列中取出任务对象，然后调用任务对象的逻辑处理函数完成客户请求任务
 template <typename T>
-void threadpool<T>::run() 
+void threadpool<T>::run()
 {
-    while (!m_stop)
+    while (true)
     {
-        m_queuestat.wait();//等待信号量大于0
-        m_queuelocker.lock();//等待信号量大于0
+        m_queuestat.wait();
+        m_queuelocker.lock();
         if (m_workqueue.empty())
         {
             m_queuelocker.unlock();
             continue;
         }
-        T *request = m_workqueue.front();//取出任务对象
+        T *request = m_workqueue.front();
         m_workqueue.pop_front();
         m_queuelocker.unlock();
         if (!request)
             continue;
-        request->mysql = m_connPool->GetConnection();
-        request->process();//处理客户任务
-        m_connPool->ReleaseConnection(request->mysql);
+
+        connectionRAII mysqlcon(&request->mysql, m_connPool);
+        
+        request->process();
     }
 }
 #endif
